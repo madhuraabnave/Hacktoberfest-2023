4bb7caecaa894770cdcf82dad84fa84c7bd2ad5f
madhura <madhuabnave@gmail.com>
2023-10-01 04:32:59 +0530

    Update Tech.jsx

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -10,6 +10,9 @@ const Tech = () => {
     <div className='flex flex-row flex-wrap justify-center gap-10'>
     {technologies.map((technology) => (
       <div className='w-28 h-28' key={technology.name}>
+        <h3 className='text-white text-[20px] font-bold text-center'></h3>
+          {technology.name}
+          
         <BallCanvas icon={technology.icon} />
       </div>
     ))}
